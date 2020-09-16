06f7a2f3419edfdc2d85b3c655c5253031e53a55
Crowdbotics <team@crowdbotics.com>
2020-09-16 21:56:15 +0000

    Update models

--- backend/first_vlog_app_20343/wsgi.py
+++ backend/first_vlog_app_20343/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'first_vlog_app_20343.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "first_vlog_app_20343.settings")
 
 application = get_wsgi_application()
